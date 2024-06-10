82a9c107beeb5c17895937d2ce64e7afd09546b0
Vishmidulanjalee <135816897+Vishmidulanjalee@users.noreply.github.com>
2024-06-10 09:31:10 +0530

    Add files via upload

---
+++ Quiz/questions.js
@@ -0,0 +1,121 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "In badminton, how many players are there in a doubles match?",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The shuttlecock used in badminton has feathers and a cork base. What is it called?",
+    answer: "Birdie",
+    options: [
+      "Ball",
+      "Shuttle",
+      "Birdie",
+      "Shuttlecock"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the highest score a player can achieve in a single serve in badminton?",
+    answer: "15",
+    options: [
+      "10",
+      "15",
+      "20",
+      "30"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When was badminton officially included in the Summer Olympics?",
+    answer: "1992",
+    options: [
+      "1980",
+      "1992",
+      "2000",
+      "2016"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many points are needed to win a standard game of badminton?",
+    answer: "21",
+    options: [
+      "11",
+      "15",
+      "21",
+      "25"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "What is the name of the shot in badminton where the shuttlecock is hit forcefully downward into the opponent's court?",
+    answer: "Smash",
+    options: [
+      "Smash",
+      "Drive",
+      "Dropshot",
+      "Clear"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What is the purpose of the backhand grip in badminton?",
+    answer: "To improve control and accuracy",
+    options: [
+      "To increase power in shots",
+      "To improve control and accuracy",
+      "To confuse the opponent",
+      "To facilitate serving",
+    
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "How many times is a player allowed to hit the shuttlecock on their side of the net during a rally in badminton?",
+    answer: "Unlimited",
+    options: [
+      "1",
+      "2",
+      "3",
+      "Unlimited"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "In badminton, what is it called when the server's feet do not remain stationary during the serve?",
+    answer: "Fault",
+    options: [
+      "Fault",
+      "Let",
+      "Replay",
+      "Ace"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Which country is historically dominant in badminton and has won the most Olympic gold medals in the sport?",
+    answer: "China",
+    options: [
+      "US",
+      "China",
+      "India",
+      "Malaysia"
+    ]
+  },
+
+ 
+];
